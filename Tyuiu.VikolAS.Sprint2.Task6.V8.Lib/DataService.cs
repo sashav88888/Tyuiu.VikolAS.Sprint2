16da26933f1c59a8f56a4893463b9c2e57180e9a
alex vikol <sashavikol7@gmail.com>
2025-10-10 12:39:51 +0500

    Task6Final

--- Tyuiu.VikolAS.Sprint2.Task6.V8.Lib/DataService.cs
+++ Tyuiu.VikolAS.Sprint2.Task6.V8.Lib/DataService.cs
@@ -1,5 +1,4 @@
-﻿
-using tyuiu.cources.programming.interfaces.Sprint2;
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
 
 namespace Tyuiu.VikolAS.Sprint2.Task6.V8.Lib
 {
@@ -7,46 +6,42 @@ namespace Tyuiu.VikolAS.Sprint2.Task6.V8.Lib
     {
         public string FindDateOfPreviousDay(int m, int n)
         {
-            if (n > 1)
+
+            int day = m;
+            int month = n;
+
+            int prevDay, prevMonth;
+
+
+            if (day > 1)
             {
-                // Просто отнимаем 1 от числа, месяц остается тем же.
-                // :D2 - это форматирование, которое добавляет 0 впереди, если число меньше 10.
-                // Например, 9 станет "09".
-                return $"{(n - 1):D2}.{m:D2}";
+                prevDay = day - 1;
+                prevMonth = month;
             }
 
-            // --- Случай 2: Сегодня 1-е число месяца ---
-            // Если код дошел сюда, значит n = 1.
-            // Например, сегодня 1 марта, вчера было 28 февраля.
-
-            // Сначала вычисляем номер предыдущего месяца.
-            int prevMonth = m - 1;
-
-            // Эта переменная будет хранить последнее число предыдущего месяца.
-            int lastDayOfPrevMonth;
-
-            // Используем switch, чтобы определить, сколько дней было в *предыдущем* месяце.
-            switch (prevMonth)
+            else
             {
+                prevMonth = month - 1;
+
+                switch (prevMonth)
+                {
                     case 2:
                         prevDay = 28;
-                    break;
-
-                case 4:  // Если предыдущий - апрель,
-                case 6:  // июнь,
-                case 9:  // сентябрь,
-                case 11: // или ноябрь
-                    lastDayOfPrevMonth = 30; // В них по 30 дней
-                    break;
-
-                default: // Во всех остальных месяцах (январь, март, май и т.д.)
-                    lastDayOfPrevMonth = 31; // 31 день
-                    break;
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        prevDay = 30;
+                        break;
+                    default:
+                        prevDay = 31;
+                        break;
+                }
             }
 
-            // Возвращаем результат: последнее число предыдущего месяца и номер предыдущего месяца.
-            // Также используем форматирование :D2 для вывода в формате дд.мм.
-            return $"{lastDayOfPrevMonth:D2}.{prevMonth:D2}";
+
+            return $"{prevDay:D2}.{prevMonth:D2}";
         }
     }
 }
--- Tyuiu.VikolAS.Sprint2.Task6.V8.Test/DataServiceTest.cs
+++ Tyuiu.VikolAS.Sprint2.Task6.V8.Test/DataServiceTest.cs
@@ -6,33 +6,24 @@ public sealed class DataServiceTest
     [TestMethod]
     public void TestPreviousDayLogic()
     {
-       
+
         DataService ds = new DataService();
+        // Проверяем 15 марта -> 14 марта. Вызываем FindDateOfPreviousDay(день, месяц)
+        Assert.AreEqual("14.03", ds.FindDateOfPreviousDay(15, 3));
 
-        // --- Выполнение и Проверка ---
-        // Assert.AreEqual(ожидаемый_результат, реальный_результат)
-        // Эта команда сравнивает то, что мы ожидаем получить, с тем, что реально вернул метод.
-        // Если они совпадают - тест пройден (зеленый), если нет - провален (красный).
+        // Проверяем 1 марта -> 28 февраля
+        Assert.AreEqual("28.02", ds.FindDateOfPreviousDay(1, 3));
 
-        // Тест 1: Обычный день в середине месяца.
-        // Проверяем, что для 15 марта метод вернет "14.03".
-        Assert.AreEqual("14.03", ds.FindDateOfPreviousDay(3, 15));
+        // Проверяем 1 мая -> 30 апреля
+        Assert.AreEqual("30.04", ds.FindDateOfPreviousDay(1, 5));
 
-        // Тест 2: Переход на предыдущий месяц - Февраль (28 дней).
-        // Проверяем, что для 1 марта метод вернет "28.02".
-        Assert.AreEqual("28.02", ds.FindDateOfPreviousDay(3, 1));
+        // Проверяем 1 августа -> 31 июля
+        Assert.AreEqual("31.07", ds.FindDateOfPreviousDay(1, 8));
 
-        // Тест 3: Переход на предыдущий месяц (с 30 днями).
-        // Проверяем, что для 1 мая метод вернет "30.04".
-        Assert.AreEqual("30.04", ds.FindDateOfPreviousDay(5, 1));
+        // Проверяем 1 февраля -> 31 января
+        Assert.AreEqual("31.01", ds.FindDateOfPreviousDay(1, 2));
 
-        // Тест 4: Переход на предыдущий месяц (с 31 днем).
-        // Проверяем, что для 1 августа метод вернет "31.07".
-        Assert.AreEqual("31.07", ds.FindDateOfPreviousDay(8, 1));
-
-       
-        Assert.AreEqual("31.01", ds.FindDateOfPreviousDay(2, 1));
-
-        Assert.AreEqual("09.10", ds.FindDateOfPreviousDay(10, 10));
+        // Проверяем случай из твоей ошибки: 25 мая -> 24 мая
+        Assert.AreEqual("24.05", ds.FindDateOfPreviousDay(25, 5));
     }
 }
